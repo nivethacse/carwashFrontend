75edfd7fe3cdde25feca05e127bc0b5db2af4597
Nivetha s <akshaya2d@gmail.com>
2022-04-20 17:01:06 +0530

    image changes

--- src/components/forgetpass.js
+++ src/components/forgetpass.js
@@ -51,7 +51,7 @@ function ForgetPass(){
         <div className="loginDiv">
             <div className="loginForm">
                 <div style={{width:"auto"}}>
-                    <img src="./images/shops.png" alt="c" className="logInImages"/>
+                    <img src="./images/toolsPic.jpg" alt="c" className="logInImages"/>
                     {/* <p>yess done</p> */}
                 </div>
                 <div style={{width:"100%"}}>
